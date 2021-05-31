75ee6cd6f1377f5ebd735b2a68db194cda4fee38
Shuttler22 <76698735+Shuttler22@users.noreply.github.com>
2021-05-31 20:29:56 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -16,32 +16,32 @@ function setup() {
 
 
    for (var k = 0; k <=width; k = k + 80) {
-     divisions = new Divisions (k, height-divisionHeight/2, 10, divisionHeight);
+     divisions.push(new Divisions (k, height-divisionHeight/2, 10, divisionHeight));
    }
 
 
     for (var j = 75; j <=width; j=j+50) 
     {
     
-       plinkos = new Plinko(j,75);
+       plinkos.push(new Plinko(j,75));
     }
 
     for (var j = 50; j <=width-10; j=j+50) 
     {
     
-       plinkos = new Plinko(j,175);
+       plinkos.push(new Plinko(j,175));
     }
 
      for (var j = 75; j <=width; j=j+50) 
     {
     
-       plinkos = new Plinko(j,275);
+       plinkos.push(new Plinko(j,275));
     }
 
      for (var j = 50; j <=width-10; j=j+50) 
     {
     
-       plinkos = new Plinko(j,375);
+       plinkos.push(new Plinko(j,375));
     }
 
     
@@ -69,7 +69,7 @@ function draw() {
    }
  
   for (var j = 0; j < particles.length; j++) {
-   
+     
      particles[j].display();
    }
    for (var k = 0; k < divisions.length; k++) {
